7ccfae542cf4d91ea9ab28cf403305dda40c6d1d
emphibian <ryuusleek@protonmail.ch>
2025-06-28 20:57:35 +0530

    Make React development server work with express

--- server/app.js
+++ server/app.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const path = require("path");
 const port = 5000;
 
+if (process.env.NODE_ENV === "development") {
+	app.use(cors({ origin: true }));
+}
+
 app.use(express.static(path.join(__dirname, "/dist")));
+
+app.get("/api/test", (req, res) => {
+	res.json({ message: "Request received" });
+});
 
 app.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname, "/dist/index.html"));
