1e9901b4994cbcf898306c5181757ffc17dae70f
emphibian <ryuusleek@protonmail.ch>
2025-06-29 20:12:24 +0530

    Set up basic React routing

---
+++ react/src/Landing.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router";
+
+export function Landing() {
+	return (
+		<>
+			<h1>Habit Tracker App</h1>
+			<p>
+				Proceed to <Link to="/register">Register</Link>
+			</p>
+		</>
+	);
+}
--- react/src/main.jsx
+++ react/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+import { Landing } from "./Landing";
+import { Register } from "./Register";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Landing />} />
+				<Route path="/register" element={<Register />} />
+			</Routes>
+		</BrowserRouter>
+	</StrictMode>,
+);
