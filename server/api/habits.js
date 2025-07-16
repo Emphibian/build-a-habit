7f92431928856b19c2758432fb378e1bbb55487d
emphibian <ryuusleek@protonmail.ch>
2025-07-16 20:59:33 +0530

    Add API endpoint for updating work duration

--- server/api/habits.js
+++ server/api/habits.js
@@ -43,7 +43,6 @@ router.patch("/habit/completed/:id", async (req, res) => {
 		const userId = req.session.user.id;
 		const habitInstance = await Instance.findById(req.params.id).exec();
 
-		console.log({ target: habitInstance.goalTarget, value: req.body.value });
 		if (habitInstance.goalType === "yes/no") {
 			habitInstance.status =
 				habitInstance.status === "Completed" ? "Not Completed" : "Completed";
@@ -56,6 +55,26 @@ router.patch("/habit/completed/:id", async (req, res) => {
 			habitInstance.goalValue = req.body.value;
 		}
 
+		await habitInstance.save();
+		res.json(habitInstance);
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ message: "Internal Server Error" });
+	}
+});
+
+router.patch("/habit/addDuration/:id", async (req, res) => {
+	try {
+		if (!req.session.user) {
+			res.status(401).json({ message: "Not Logged In" });
+			return;
+		}
+
+		const userId = req.session.user.id;
+		const habitInstance = await Instance.findById(req.params.id).exec();
+		const value = parseInt(req.body.value);
+
+		habitInstance.workDuration = habitInstance.workDuration + value;
 		await habitInstance.save();
 		res.json(habitInstance);
 	} catch (error) {
