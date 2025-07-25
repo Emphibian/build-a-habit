0b23fc63cd796f906cf14b8c00888722eeed8d86
emphibian <ryuusleek@protonmail.ch>
2025-07-25 19:14:22 +0530

    Display worked on duration for tasks

--- react/src/home/Habit.jsx
+++ react/src/home/Habit.jsx
@@ -39,11 +39,20 @@ function DoneModal({ isOpen, setOpen, handleHabitUpdate }) {
 	);
 }
 
-function Habit({ name, handleUpdate, handleTimer }) {
+function Habit({ name, workDuration, handleUpdate, handleTimer }) {
+	const prettyPrintDuration = function (workDuration) {
+		if (workDuration < 60) return `${workDuration}m`;
+		const hours = Math.floor(workDuration / 60);
+		const minutes = workDuration % 60;
+
+		return `${hours}h ${minutes}m`;
+	};
+
 	const fill = "#3B4554";
 	return (
 		<div className="habit">
 			<p className="habit-name">{name}</p>
+			<span>{prettyPrintDuration(workDuration)}</span>
 			<button onClick={handleUpdate}>
 				<Checkmark fill={fill} />
 			</button>
@@ -187,6 +196,7 @@ export function Habits() {
 							<Habit
 								key={habit._id}
 								name={habit.name}
+								workDuration={habit.workDuration}
 								handleUpdate={() =>
 									updateValue(
 										habit._id,
