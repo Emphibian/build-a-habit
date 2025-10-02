6add6f57c4479b768aa9cecfc0345c89b6394b28
emphibian <adarshrajput181@gmail.com>
2025-10-02 23:00:07 +0530

    Add field for re-enter password in registration

--- react/src/pages/Register.jsx
+++ react/src/pages/Register.jsx
@@ -5,13 +5,28 @@ import userAPI from "../api/userAPI.js";
 export function Register() {
 	const [username, setUsername] = useState("");
 	const [password, setPassword] = useState("");
+	const [reenterPassword, setReenterPassword] = useState("");
 	const [responseMessage, setResponseMessage] = useState("");
+	const [errorMessage, setErrorMessage] = useState(null);
 
 	const handleRegister = async function(event) {
 		event.preventDefault();
+		// check if password is the same as re-enter password
+		if (password !== reenterPassword) {
+			setErrorMessage("Passwords do not match");
+			return;
+		}
+
+		setErrorMessage(null);
 		const response = await userAPI.register(username, password);
 		const data = await response.json();
-		setResponseMessage(data.message);
+
+		// account successfully created
+		if (response.status === 201) {
+			setResponseMessage(data.message);
+		} else {
+			setErrorMessage(data.message);
+		}
 	};
 
 	return (
@@ -22,6 +37,9 @@ export function Register() {
 				<div className="placeholder-form">
 					<div className="form-div">
 						<h2 className="mb-05">Register</h2>
+						{errorMessage !== null && (
+							<div className="error">{errorMessage}</div>
+						)}
 						<form onSubmit={handleRegister}>
 							<label>
 								<input
@@ -34,12 +52,21 @@ export function Register() {
 							</label>
 							<label>
 								<input
-									type="text"
+									type="password"
 									value={password}
 									onChange={(event) => setPassword(event.target.value)}
 									required
 								/>
 								<span>Password</span>
+							</label>
+							<label>
+								<input
+									type="password"
+									value={reenterPassword}
+									onChange={(event) => setReenterPassword(event.target.value)}
+									required
+								/>
+								<span>Re-enter Password</span>
 							</label>
 							<button type="submit">Register</button>
 						</form>
