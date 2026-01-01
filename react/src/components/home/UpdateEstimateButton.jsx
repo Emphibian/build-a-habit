4f4bfb4d878cbde960ffaedf9e4946e51c6271cc
emphibian <adarshrajput181@gmail.com>
2026-01-01 20:00:02 +0530

    Fix: time tracking modal was not being centered

--- react/src/components/home/UpdateEstimateButton.jsx
+++ react/src/components/home/UpdateEstimateButton.jsx
@@ -2,100 +2,103 @@ import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import {
-  updateEstimate,
-  updateTimeSpent,
+	updateEstimate,
+	updateTimeSpent,
 } from "../../features/habits/habitsThunks";
 import { setTaskEstimate, setTaskTime } from "../../features/tasks/tasksThunks";
 
 import { Dial } from "./Dial.jsx";
 import StopwatchIcon from "../../assets/svgs/timer.svg?react";
+import { createPortal } from "react-dom";
 
 export function UpdateEstimateButton({ id, isHabit }) {
-  const [modalOpen, setModalOpen] = useState(false);
-  const [estimate, setEstimate] = useState(0);
-  const [timeSpent, setTimeSpent] = useState(0);
-  const dispatch = useDispatch();
+	const [modalOpen, setModalOpen] = useState(false);
+	const [estimate, setEstimate] = useState(0);
+	const [timeSpent, setTimeSpent] = useState(0);
+	const dispatch = useDispatch();
 
-  const initialEstimate = useSelector((state) =>
-    isHabit
-      ? state.habits.byId[id].timeEstimate
-      : state.tasks.byId[id].timeEstimate,
-  );
+	const initialEstimate = useSelector((state) =>
+		isHabit
+			? state.habits.byId[id].timeEstimate
+			: state.tasks.byId[id].timeEstimate,
+	);
 
-  const initialTimeSpent = useSelector((state) =>
-    isHabit
-      ? state.habits.byId[id].workDuration
-      : state.tasks.byId[id].workDuration,
-  );
+	const initialTimeSpent = useSelector((state) =>
+		isHabit
+			? state.habits.byId[id].workDuration
+			: state.tasks.byId[id].workDuration,
+	);
 
-  useEffect(() => {
-    setEstimate(initialEstimate);
-    setTimeSpent(initialTimeSpent);
-  }, []);
+	useEffect(() => {
+		setEstimate(initialEstimate);
+		setTimeSpent(initialTimeSpent);
+	}, []);
 
-  const handleUpdate = async function(event) {
-    event.preventDefault();
-    if (isHabit) {
-      if (initialEstimate != estimate) {
-        await dispatch(updateEstimate({ id, estimate }));
-      }
-      if (initialTimeSpent != timeSpent) {
-        await dispatch(updateTimeSpent({ id, timeSpent }));
-      }
-    } else {
-      if (initialEstimate != estimate)
-        await dispatch(setTaskEstimate({ id, estimate }));
-      if (initialTimeSpent != timeSpent)
-        await dispatch(setTaskTime({ id, timeSpent }));
-    }
+	const handleUpdate = async function (event) {
+		event.preventDefault();
+		if (isHabit) {
+			if (initialEstimate != estimate) {
+				await dispatch(updateEstimate({ id, estimate }));
+			}
+			if (initialTimeSpent != timeSpent) {
+				await dispatch(updateTimeSpent({ id, timeSpent }));
+			}
+		} else {
+			if (initialEstimate != estimate)
+				await dispatch(setTaskEstimate({ id, estimate }));
+			if (initialTimeSpent != timeSpent)
+				await dispatch(setTaskTime({ id, timeSpent }));
+		}
 
-    closeModal();
-  };
+		closeModal();
+	};
 
-  const openModal = function() {
-    setModalOpen(true);
-  };
+	const openModal = function () {
+		setModalOpen(true);
+	};
 
-  const closeModal = function() {
-    setModalOpen(false);
-  };
+	const closeModal = function () {
+		setModalOpen(false);
+	};
 
-  let modal = "";
-  if (modalOpen) {
-    modal = (
-      <div className="habit-overlay" onClick={closeModal}>
-        <div className="main-habit-form" onClick={(e) => e.stopPropagation()}>
-          <h3>Duration</h3>
-          <form onSubmit={handleUpdate}>
-            <div className="justify-center gap-15">
-              <Dial
-                initial={initialEstimate}
-                onChange={setEstimate}
-                label={"Estimate"}
-              />
-              <Dial
-                initial={initialTimeSpent}
-                onChange={setTimeSpent}
-                label={"Time Spent"}
-              />
-            </div>
-            <div className="btn-grp">
-              <button type="submit">Update</button>
-              <button onClick={closeModal}>Cancel</button>
-            </div>
-          </form>
-        </div>
-      </div>
-    );
-  }
+	let modal = "";
+	if (modalOpen) {
+		const modalDiv = (
+			<div className="habit-overlay" onClick={closeModal}>
+				<div className="main-habit-form" onClick={(e) => e.stopPropagation()}>
+					<h3>Duration</h3>
+					<form onSubmit={handleUpdate}>
+						<div className="justify-center gap-15">
+							<Dial
+								initial={initialEstimate}
+								onChange={setEstimate}
+								label={"Estimate"}
+							/>
+							<Dial
+								initial={initialTimeSpent}
+								onChange={setTimeSpent}
+								label={"Time Spent"}
+							/>
+						</div>
+						<div className="btn-grp">
+							<button type="submit">Update</button>
+							<button onClick={closeModal}>Cancel</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		);
 
-  return (
-    <>
-      {modal}
-      <button onClick={openModal}>
-        <StopwatchIcon />
-        <span>Time Tracking</span>
-      </button>
-    </>
-  );
+		modal = createPortal(modalDiv, document.getElementById("modal-root"));
+	}
+
+	return (
+		<>
+			{modal}
+			<button onClick={openModal}>
+				<StopwatchIcon />
+				<span>Time Tracking</span>
+			</button>
+		</>
+	);
 }
