9d5c8b76ea2b01770a2cf17a75a1636354d83e89
emphibian <adarshrajput181@gmail.com>
2026-01-21 00:20:55 +0530

    Sign JWT Access Token for login

--- server/api/login.js
+++ server/api/login.js
@@ -5,6 +5,7 @@ const bcrypt = require("bcryptjs");
 const User = require("../models/usersModel.js");
 const generateInstances = require("../utils/generateInstances.js");
 const generateDayString = require("../utils/generateDayString.js");
+const jwt = require("jsonwebtoken");
 
 router.post("/login", async (req, res) => {
 	try {
@@ -15,28 +16,43 @@ router.post("/login", async (req, res) => {
 			res.status(401).json({ message: "Username or Password doesn't match" });
 		}
 
-		const match = await bcrypt.compare(password, storedUser.passwordHash);
+		const match = bcrypt.compare(password, storedUser.passwordHash);
 
-		if (match) {
-			const currentDay = new Date();
-			currentDay.setHours(0, 0, 0, 0);
-			req.session.user = {
-				id: storedUser._id,
-				lastAccess: generateDayString(currentDay),
-			};
-
-			// logging in for the first time today
-			if (storedUser.lastLogin.getTime() !== currentDay.getTime()) {
-				storedUser.lastLogin = currentDay;
-				storedUser.todayDuration = 0;
-				await storedUser.save();
-				generateInstances(storedUser._id);
-			}
-
-			res.status(201).json({ message: "Logged In" });
-		} else {
+		if (!match) {
 			res.status(401).json({ message: "Username or Password doesn't match" });
 		}
+
+		const currentDay = new Date();
+		currentDay.setHours(0, 0, 0, 0);
+		req.session.user = {
+			id: storedUser._id,
+			lastAccess: generateDayString(currentDay),
+		};
+
+		// make a jwt token for this sign up
+		const token = jwt.sign(
+			{ id: storedUser._id, lastAccess: generateDayString(currentDay) },
+			"secret", // WARNING: too simple of a secret
+			{ expiresIn: "8h" },
+		);
+
+		// WARNING: these settings should be changed at deployment
+		res.cookie("token", token, {
+			httpOnly: true,
+			secure: false,
+			sameSite: "Lax",
+			maxAge: 60 * 60 * 1000 * 8, // 8 hours
+		});
+
+		// logging in for the first time today
+		if (storedUser.lastLogin.getTime() !== currentDay.getTime()) {
+			storedUser.lastLogin = currentDay;
+			storedUser.todayDuration = 0;
+			await storedUser.save();
+			generateInstances(storedUser._id);
+		}
+
+		res.status(201).json({ message: "Logged In", token });
 	} catch (error) {
 		console.log(error);
 		res.status(500).json({ message: "Internal Server Error" });
