9055dc2188e3f73bd962f9f50b125d49f0c8e4eb
emphibian <adarshrajput181@gmail.com>
2026-01-30 01:15:43 +0530

    Add a timer screen to focus session

--- react/src/components/home/operations/FocusSessionButton.jsx
+++ react/src/components/home/operations/FocusSessionButton.jsx
@@ -1,57 +1,112 @@
-import { useState } from "react";
+import { useState, useEffect, useContext, useRef } from "react";
 import { createPortal } from "react-dom";
 
+import { TimerContext } from "../../../contexts/TimerContext";
 import { Dial } from "../Dial";
 import StopwatchIcon from "../../../assets/svgs/timer.svg?react";
 import CloseIcon from "../../../assets/svgs/close.svg?react";
 
-const FocusModal = ({ closeModal }) => {
-  const [duration, setDuration] = useState(25);
+const InitialScreen = ({ duration, setDuration, start }) => {
+	return (
+		<>
+			<h3>Set Focus Session Duration</h3>
+			<Dial initial={duration} onChange={(duration) => setDuration(duration)} />
+			<div>
+				<button className="button-blue" onClick={() => start()}>
+					Start Session
+				</button>
+			</div>
+		</>
+	);
+};
 
-  return (
-    <div className="focus-modal">
-      <div className="focus-div">
-        <h3>Set Focus Session Duration</h3>
-        <Dial
-          initial={duration}
-          onChange={(duration) => setDuration(duration)}
-        />
-        <div>
-          <button className="button-blue">Start Session</button>
-        </div>
-      </div>
-      <button onClick={() => closeModal()} className="focus-close">
-        <CloseIcon />
-      </button>
-    </div>
-  );
+const prettyPrintTime = (time) => {
+	const minutes = Math.floor(time / 60);
+	const seconds = time % 60 > 9 ? time % 60 : `0${time % 60}`;
+
+	return `${minutes}:${seconds}`;
+};
+
+const TimerScreen = ({ duration }) => {
+	const [timeLeft, setTimeLeft] = useState(duration * 60);
+	const intervalRef = useRef(null);
+
+	useEffect(() => {
+		if (!intervalRef.current) {
+			intervalRef.current = setInterval(
+				() => setTimeLeft((prev) => prev - 1),
+				1000,
+			);
+		}
+
+		return () => {
+			if (timeLeft <= 1) clearInterval(intervalRef.current);
+		};
+	}, [timeLeft]);
+
+	return (
+		<>
+			<div className="focus-time">
+				<p>Time Left</p>
+				<span>{prettyPrintTime(timeLeft)}</span>
+			</div>
+			<button className="button-blue">Finish Task</button>
+		</>
+	);
+};
+
+const FocusModal = ({ closeModal }) => {
+	const [duration, setDuration] = useState(1);
+	const [currentScreen, setCurrentScreen] = useState("initial");
+
+	let modal = "";
+	if (currentScreen == "initial") {
+		modal = (
+			<InitialScreen
+				duration={duration}
+				setDuration={setDuration}
+				start={() => setCurrentScreen("timer")}
+			/>
+		);
+	} else if (currentScreen == "timer") {
+		modal = <TimerScreen duration={duration} />;
+	}
+
+	return (
+		<div className="focus-modal">
+			<div className="focus-div">{modal}</div>
+			<button onClick={() => closeModal()} className="focus-close">
+				<CloseIcon />
+			</button>
+		</div>
+	);
 };
 
 export const FocusSessionButton = () => {
-  const [modalOpen, setModalOpen] = useState(false);
+	const [modalOpen, setModalOpen] = useState(false);
 
-  const openModal = () => {
-    setModalOpen(true);
-  };
+	const openModal = () => {
+		setModalOpen(true);
+	};
 
-  const closeModal = () => {
-    setModalOpen(false);
-  };
+	const closeModal = () => {
+		setModalOpen(false);
+	};
 
-  let modal = "";
-  if (modalOpen) {
-    let domElement = <FocusModal closeModal={closeModal} />;
-    modal = createPortal(domElement, document.getElementById("modal-root"));
-  }
+	let modal = "";
+	if (modalOpen) {
+		let domElement = <FocusModal closeModal={closeModal} />;
+		modal = createPortal(domElement, document.getElementById("modal-root"));
+	}
 
-  return (
-    <>
-      {modal}
-      <button onClick={openModal}>
-        {/* TODO: change this icon */}
-        <StopwatchIcon />
-        <span>Start Focus Session</span>
-      </button>
-    </>
-  );
+	return (
+		<>
+			{modal}
+			<button onClick={openModal}>
+				{/* TODO: change this icon */}
+				<StopwatchIcon />
+				<span>Start Focus Session</span>
+			</button>
+		</>
+	);
 };
